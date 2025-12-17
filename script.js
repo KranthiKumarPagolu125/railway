985ee8dc7b928d9867d51f47740aa534dbefab63
KranthiKumarPagolu125 <pagolukranthikumar125@gmail.com>
2025-12-17 07:41:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
 function bookTicket() {
-    var trainNo = document.getElementById("train").value;
-
     fetch("/book", {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-            train_no: trainNo
+            train_no: document.getElementById("train").value,
+            name: document.getElementById("name").value,
+            age: document.getElementById("age").value,
+            passengers: document.getElementById("passengers").value,
+            berth: document.getElementById("berth").value
         })
     })
-    .then(response => response.json())
+    .then(res => res.json())
     .then(data => {
         if (data.error) {
-            document.getElementById("result").innerText = data.error;
+            result.innerText = data.error;
         } else {
-            document.getElementById("result").innerHTML =
-                "✅ Ticket Booked<br>" +
-                "Train: " + data.train + "<br>" +
-                "Fare: ₹" + data.fare + "<br>" +
-                "PNR: " + data.pnr;
+            result.innerHTML = "✅ Ticket Booked<br>PNR: " + data.pnr;
         }
     });
 }
 
+function viewTicket() {
+    let pnr = document.getElementById("pnr").value;
+    fetch("/view/" + pnr)
+    .then(res => res.json())
+    .then(data => {
+        if (data.error) {
+            result.innerText = data.error;
+        } else {
+            result.innerHTML =
+                "Train: " + data.train + "<br>" +
+                "Name: " + data.name + "<br>" +
+                "Passengers: " + data.passengers + "<br>" +
+                "Berth: " + data.berth + "<br>" +
+                "Fare: ₹" + data.fare;
+        }
+    });
+}
 
+function cancelTicket() {
+    let pnr = document.getElementById("pnr").value;
+    fetch("/cancel/" + pnr)
+    .then(res => res.json())
+    .then(data => {
+        result.innerText = data.message || data.error;
+    });
+}
